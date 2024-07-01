3d3ae9191119eeacc39aeb8fc27e09fdc8e189a7
Stanislav Kosorin <s.kosorin@campus.tu-berlin.de>
2024-07-01 22:00:34 +0200

    GCP Terraform File (#11)
    * Working deployment without edge ip
    
    * Add ip & port config to cloud
    
    * Add IP env var to terraform
    
    * Update readme

--- cloud_component/main.py
+++ cloud_component/main.py
@@ -6,7 +6,6 @@ from concurrent import futures
 import time
 import grpc
 from grpc_reflection.v1alpha import reflection
-import queue
 import threading
 from uuid import uuid4
 
@@ -17,10 +16,11 @@ if project_root not in sys.path:
 import proto.fog_pb2 as fog_pb2  # noqa
 import proto.fog_pb2_grpc as fog_pb2_grpc  # noqa
 
+
 class Task:
-    def __init__(self,value, task_id=None, result= None, processed= False):
+    def __init__(self, value, task_id=None, result=None, processed=False):
         if task_id is None:
-            task_id=uuid4()
+            task_id = uuid4()
         self.id = task_id
         self.value = value
         self.result = result
@@ -31,21 +31,29 @@ class Task:
             "id": str(self.id),
             "value": self.value,
             "result": self.result,
-            "processed": self.processed
+            "processed": self.processed,
         }
 
     @classmethod
     def from_dict(cls, data):
-        return cls(value=data["value"], task_id=data["id"], result=data["result"], processed=data["processed"])
+        return cls(
+            value=data["value"],
+            task_id=data["id"],
+            result=data["result"],
+            processed=data["processed"],
+        )
 
     def __str__(self):
-        return f'Task_id: {self.id}, processed: {self.processed}'
-    
+        return f"Task_id: {self.id}, processed: {self.processed}"
+
+
 class TaskQueue:
     def __init__(self):
         self.tasks = deque()
         self.lock = threading.Lock()
-        self.persistence_file_path = os.path.join(os.path.dirname(__file__), "queue_replica.json")
+        self.persistence_file_path = os.path.join(
+            os.path.dirname(__file__), "queue_replica.json"
+        )
 
         self.load_tasks_from_replica_queue()
 
@@ -53,7 +61,7 @@ class TaskQueue:
         if not os.path.exists(self.persistence_file_path):
             with open(self.persistence_file_path, "w") as f:
                 print("creating replica file")
-                res = dump([], f)
+                dump([], f)
 
     def load_tasks_from_replica_queue(self):
         if os.path.exists(self.persistence_file_path):
@@ -69,10 +77,10 @@ class TaskQueue:
         if os.path.exists(self.persistence_file_path):
             with open(self.persistence_file_path, "w") as f:
                 task_list = [task.to_dict() for task in self.tasks]
-                res = dump(task_list, f)
+                dump(task_list, f)
         else:
             print("replica file not found!")
-                
+
     def add_task(self, task: Task):
         with self.lock:
             self.tasks.append(task)
@@ -99,56 +107,52 @@ class TaskQueue:
             else:
                 return None
 
-            
+
 class CloudService(fog_pb2_grpc.CloudServiceServicer):
     def __init__(self):
         self.task_queue = TaskQueue()
-        self.feedback_url = "placeholder_url"
         self.task_queue.init_persistence_file()
+        self.edge_ip = os.getenv("EDGE_IP", "localhost")
+        self.edge_port = os.getenv("EDGE_PORT", 50052)
 
     def ProcessData(self, request: fog_pb2.ProcessDataRequest, context):
-        response_data = fog_pb2.Position(x=10.0, y=20.0, z=0.5)
         print(f"Received data: {request.data}")
         if not request.ListFields():
             print("Data empty!")
 
         task = Task(value=request.data.value)
         self.task_queue.add_task(task=task)
-        
+
         return fog_pb2.ProcessDataResponse()
 
     def process_task_queue(self):
         while True:
             task_peek = self.task_queue.peek_task()
             if task_peek:
-                if task_peek.processed == False:
+                if not task_peek.processed:
                     self.process_task(task=task_peek)
                 feedback_success = self.send_feedback(task=task_peek)
-                if feedback_success == True:
+                if feedback_success:
                     self.task_queue.get_task()
-                    
+
                 time.sleep(1)
             else:
                 time.sleep(1)
 
     def process_task(self, task: Task):
         task_value = task.value
-        
+
         task.result = task_value
         task.processed = True
         return task
-    
+
     def send_feedback(self, task):
         print("sending feedback")
-        channel = grpc.insecure_channel('localhost:50052')
+        channel = grpc.insecure_channel(f"{self.edge_ip}:{self.edge_port}")
         stub = fog_pb2_grpc.EdgeServiceStub(channel)
-        
+
         # TODO send actual data
-        result_data = {
-            'x' : 4,
-            'y' : 5,
-            'z' : 6
-        }
+        result_data = fog_pb2.Position(x=4, y=5, z=6)
 
         request = fog_pb2.UpdatePositionRequest(position=result_data)
         try:
@@ -156,7 +160,10 @@ class CloudService(fog_pb2_grpc.CloudServiceServicer):
             return True
         except grpc.RpcError as e:
             status_code = e.code()
-            print(f"Request to Edge Service failed with error: {status_code.name} ({status_code.value})")
+            print(
+                f"Request to Edge Service failed with error:"
+                f"{status_code.name} ({status_code.value})"
+            )
 
 
 def serve():
@@ -176,12 +183,13 @@ def serve():
     server.start()
     print("Server started on port 50051")
 
-    task_processor_thread = threading.Thread(target=cloud_service.process_task_queue)
+    task_processor_thread = threading.Thread(
+        target=cloud_service.process_task_queue
+    )
     task_processor_thread.start()
-    print("Started Queue Processor on seperate Thread")
+    print("Started Queue Processor on separate Thread")
 
     server.wait_for_termination()
-
 
 
 if __name__ == "__main__":
