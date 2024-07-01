0c01f246d27d03151a635d348dbce0a378f69318
s.kosorin <s.kosorin@campus.tu-berlin.de>
2024-07-01 21:35:15 +0200

    Add IP env var to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "google_compute_instance" "default" {
     git clone https://github.com/LeanderAK/SS24_FOGC_Project.git /opt/SS24_FOGC_Project
     cd /opt/SS24_FOGC_Project
     pip3 install -r requirements.txt
-    make cloud &
+    EDGE_IP=<add_your_ip> make cloud &
   EOT
 }
 
